be8bf4ae8114723269cc50cd02d4c7432f0cf4c4
kuladip chaudhari <kuladip@bizminds.io>
2019-03-31 09:05:25 +0530

    added models

---
+++ Bizminds.Models/Palindrome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bizminds.Models
+{
+    public class PalindromeRequest
+    {
+        [Display(Name = "Palindrome", Description = "Palindrome String")]
+        [Required()]
+        public string Value { get; set; }
+    }
+
+}
