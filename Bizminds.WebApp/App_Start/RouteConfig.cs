0c8115a27bdc6d65139e85a21e2b8ce27582b23a
kuladip chaudhari <kuladip@bizminds.io>
2019-03-31 09:10:48 +0530

    Removed unwanted stuff

--- Bizminds.WebApp/App_Start/RouteConfig.cs
+++ Bizminds.WebApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Bizminds.WebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Palindrome", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
